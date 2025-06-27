d9ab7c1751943413d476526699f7ead926845343
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-27 16:08:23 +0200

    API constanten, de endpoint URL gedeclareerd, geinitialiseerd en klaargezet voor export

---
+++ src/constants/apiConstants.js
@@ -0,0 +1,4 @@
+export const NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything';
+export const WEATHER_API_BASE_URL = 'https://api.weatherapi.com/v1/current.json';
+export const OPENF1_API_BASE_URL = 'https://api.openf1.org/v1';
+export const NOVI_API_BASE_URL = 'https://novi.datavortex.nl';
