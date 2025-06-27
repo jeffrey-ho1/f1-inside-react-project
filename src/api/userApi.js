986c72dde7edf7d1b86f5b62d0b771cb34030344
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-28 00:13:12 +0200

    functie gedefinieerd voor het bijwerken van een geregistreerde user en te overschrijven

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,3 +7,17 @@ const getAuthConfig = (token) => ({
         Authorization: `Bearer ${token}`
     }
 });
+
+// Functie om gebruikersdata op te halen.
+export async function getUserData(token) {
+    // GET-request naar de /users/me endpoint, en voeg de autorisatie-header toe.
+    const response = await axios.get(`${NOVI_API_BASE_URL}/users/me`, getAuthConfig(token));
+    return response.data;
+}
+
+// Functie om gebruikersdata (zoals favorieten) bij te werken.
+export async function updateUserData(token, data) {
+    // PUT-request om de 'info'-data van de gebruiker te overschrijven.
+    const response = await axios.put(`${NOVI_API_BASE_URL}/users/me`, { info: data }, getAuthConfig(token));
+    return response.data;
+}
