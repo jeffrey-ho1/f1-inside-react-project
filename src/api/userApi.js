f2810515915a5c13a9d908381adc167f577f3ede
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-27 23:59:18 +0200

    functie aangemaakt voor de autorisatie header

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,2 +1,9 @@
 import axios from 'axios';
 import { NOVI_API_BASE_URL } from '../constants/apiConstants';
+
+// Herbruikbare functie voor autorisatie header
+const getAuthConfig = (token) => ({
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+});
