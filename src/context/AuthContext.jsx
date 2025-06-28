bef5517f522bb866219d4b66b4334443b51b5fd0
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-28 23:57:42 +0200

    import statement voor context en state geinstalleerd

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,6 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import { loginUser as loginUserApi } from '../api/authApi';
+
+
