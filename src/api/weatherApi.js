8aa9a9464fe356ddd150db8bde62359c2d541c98
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-28 16:44:41 +0200

    asynchrone functie geimplementeerd voor ophalen van  weer data via weather api

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -10,3 +10,34 @@ import { WEATHER_API_BASE_URL } from '../constants/apiConstants';
 //Stap 7: Verstuur een GET-request naar de WeatherAPI.
 //Stap 8: Geef de volledige data response terug.
 //Stap 9: Vang fouten af en log de specifieke foutmelding van de WeatherAPI.
+
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const circuitToCityMap = {
+    'Silverstone': 'Silverstone',
+    'Spa-Francorchamps': 'Francorchamps',
+    'Zandvoort': 'Zandvoort',
+    'Monza': 'Monza'
+};
+export async function getWeatherForCircuit(circuitShortName) {
+    if (!WEATHER_API_KEY) throw new Error("WeatherAPI.com sleutel niet gevonden.");
+    const city = circuitToCityMap[circuitShortName];
+    if (!city) throw new Error(`Geen stad gevonden voor circuit: ${circuitShortName}`);
+
+    try {
+        const response = await axios.get(WEATHER_API_BASE_URL, {
+            params: {
+                key: WEATHER_API_KEY,
+                q: city,
+                lang: 'nl'
+            }
+        });
+        return response.data;
+    }
+    catch (error) {
+        console.error("Fout bij weer API:", error.response?.data?.error?.message || error.message);
+        throw error;
+    }
+}
+
+
+
