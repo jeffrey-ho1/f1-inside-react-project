27bcd70cd8de33f287433ffc24cc66c24417e137
jeffrey-ho1 <ho2281@yahoo.com>
2025-06-28 16:28:57 +0200

    stappen geschreven om weer data op te halen bij de weather api

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,2 +1,12 @@
 import axios from 'axios';
 import { WEATHER_API_BASE_URL } from '../constants/apiConstants';
+
+//Stap 1: Haal de API-sleutel op uit de environment variables.
+//Stap 2: Een array om de circuitnaam van de F1 API om te zetten naar een stadsnaam volgens WeatherAPI data
+//Stap 3: Exporteer functie die het actuele weer voor een specifiek circuit ophaalt.
+//stap 4: Controleer of de sleutel aanwezig is.
+//stap 5: Zoek de bijbehorende stad op in de array.
+//Stap 6: Als de stad niet gevonden wordt, geef een duidelijke foutmelding.
+//Stap 7: Verstuur een GET-request naar de WeatherAPI.
+//Stap 8: Geef de volledige data response terug.
+//Stap 9: Vang fouten af en log de specifieke foutmelding van de WeatherAPI.
