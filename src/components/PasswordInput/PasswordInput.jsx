94c2ca1b2e2702d38a30c322f63b6564f474357e
jeffrey-ho1 <ho2281@yahoo.com>
2025-07-04 16:24:44 +0200

    feature/authentication-pages: herbruikbare component aangemaakt voor wachtwoord invoer

---
+++ src/components/PasswordInput/PasswordInput.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styles from './PasswordInput.module.css';
+import EyeOpenIcon from '../../assets/eye-open.svg?react';
+import EyeClosedIcon from '../../assets/eye-closed.svg?react';
+
+
+function PasswordInput({ value, onChange, placeholder }) {
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => {
+        console.log("TEST: Wachtwoord zichtbaarheid wordt gewijzigd naar:", !isVisible);
+        setIsVisible(prevIsVisible => !prevIsVisible);
+    };
+//
+    return (
+        <div className={styles.passwordWrapper}>
+            <input
+                type={isVisible ? 'text' : 'password'}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                required
+                className={styles.inputField}
+            />
+            <button type="button" onClick={toggleVisibility} className={styles.eyeButton}>
+                {isVisible ? <EyeClosedIcon /> : <EyeOpenIcon />}
+            </button>
+        </div>
+    );
+}
+
+export default PasswordInput;
